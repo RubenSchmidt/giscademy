41b4b99de7e57aa00ed83d5f21186eb2ead2d418
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-10-09 20:49:08 +0200

    added metric buffer helper

--- layers/models.py
+++ layers/models.py
@@ -1,4 +1,6 @@
 from django.contrib.gis.db.models import MultiPolygonField
+from django.contrib.gis.geos import MultiPolygon, Polygon
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 from giscademy.utils.model_utils import Timestampable
@@ -6,5 +8,5 @@ from giscademy.utils.model_utils import Timestampable
 
 class Layer(Timestampable):
     objtype = models.CharField(max_length=32)
-    geom = MultiPolygonField(srid=32632)
-
+    geom = MultiPolygonField()
+    json = JSONField(blank=True)
